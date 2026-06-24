14d1c7ee8241bfd3d7895ff67ea2f5eeef088b9e
Aislan Penha <aislan.penha@gmail.com>
2026-06-24 12:36:52 -0300

    Adicionando o CodeMirro e mais

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -6,5 +6,6 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.index, name='index'),
     path('post/<slug:slug>/', views.post, name='post'),
-    path('page/', views.page, name='page'),
+    path('created_by/<int:author_id>/', views.created_by, name='created_by'),
+    path('category/<slug:category_slug>/', views.category_by, name='category_by'),
 ]
--- djangoapp/blog/views/views.py
+++ djangoapp/blog/views/views.py
@@ -21,6 +21,44 @@ def index(request):
         context
     )
 
+def created_by(request, author_id):
+    # posts = Post.objects \
+    #     .filter(is_published=True) \
+    #     .order_by('-pk')
+    posts = Post.objects.get_published() \
+        .filter(created_by__pk=author_id)
+    
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj
+    }
+    return render(
+        request,
+        'blog/pages/index.html',
+        context
+    )
+
+def category_by(request, category_slug):
+    # posts = Post.objects \
+    #     .filter(is_published=True) \
+    #     .order_by('-pk')
+    posts = Post.objects.get_published() \
+        .filter(category__slug=category_slug)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context = {
+        'page_obj': page_obj
+    }
+    return render(
+        request,
+        'blog/pages/index.html',
+        context
+    )
+
 def post(request, slug):
     post = Post.objects.get_published() \
         .filter(slug=slug) \
